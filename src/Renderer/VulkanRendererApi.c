81b1e5ace691c7368e586f24c43c367058521172
Nikita Romanyuk <kelvium@yahoo.com>
2022-09-04 20:23:23 +0300

    VulkanRendererApi: implement physical device picking
    It will print some TRACE stuff about physical device groups and their
    physical devices and then pick the first discrete card, if exists. If
    not, it will fallback to the first one available. If there aren't any
    physical device groups, it will return an error and print it.

--- src/Renderer/VulkanRendererApi.c
+++ src/Renderer/VulkanRendererApi.c
@@ -8,6 +8,7 @@
 
 typedef struct VulkanGlobals {
 	VkInstance instance;
+	VkPhysicalDevice physicalDevice;
 } VulkanGlobals;
 
 static VulkanRendererApiError sVulkanCreateInstance(VulkanRendererApi* self)
@@ -55,6 +56,76 @@ static VulkanRendererApiError sVulkanCreateInstance(VulkanRendererApi* self)
 	return VULKAN_RENDERER_API_OK;
 }
 
+static VulkanRendererApiError sVulkanPickPhysicalDevice(VulkanRendererApi* self)
+{
+	VkResult result;
+
+	// get physical device group count
+	u32 physicalDeviceGroupsCount = 0;
+	result = vkEnumeratePhysicalDeviceGroups(
+			self->vulkanGlobals->instance, &physicalDeviceGroupsCount, NULL);
+	if (result != VK_SUCCESS) {
+		LOGGER_ERROR("could not enumerate physical device groups: %d\n", result);
+		return VULKAN_RENDERER_COULD_NOT_PICK_PHYSICAL_DEVICE;
+	}
+
+	if (physicalDeviceGroupsCount == 0) {
+		LOGGER_ERROR("no physical devices groups\n");
+		return VULKAN_RENDERER_NO_PHYSICAL_DEVICE_GROUPS;
+	}
+
+	// get physical device group properties
+	VkPhysicalDeviceGroupProperties* physicalDeviceGroupsProperties = calloc(
+			physicalDeviceGroupsCount, sizeof(VkPhysicalDeviceGroupProperties));
+	result = vkEnumeratePhysicalDeviceGroups(self->vulkanGlobals->instance,
+			&physicalDeviceGroupsCount, physicalDeviceGroupsProperties);
+	if (result != VK_SUCCESS) {
+		LOGGER_ERROR("could not enumerate physical device groups: %d\n", result);
+		return VULKAN_RENDERER_COULD_NOT_PICK_PHYSICAL_DEVICE;
+	}
+
+	VkPhysicalDevice fallbackPhysicalDevice = NULL;
+	for (u32 i = 0; i < physicalDeviceGroupsCount; i++) {
+		VkPhysicalDeviceGroupProperties physicalDeviceGroupProperties
+				= physicalDeviceGroupsProperties[i];
+
+		LOGGER_TRACE("physical device group properties %d:\n", i);
+		LOGGER_TRACE(" physical devices count: %d\n",
+				physicalDeviceGroupProperties.physicalDeviceCount);
+		for (u32 j = 0; j < physicalDeviceGroupProperties.physicalDeviceCount;
+				 j++) {
+			VkPhysicalDevice physicalDevice
+					= physicalDeviceGroupProperties.physicalDevices[j];
+
+			VkPhysicalDeviceProperties physicalDeviceProperties;
+			vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+			LOGGER_TRACE(" physical device %d:\n", j);
+			LOGGER_TRACE("   deviceName: %s\n", physicalDeviceProperties.deviceName);
+			LOGGER_TRACE("   api version: %d.%d.%d\n",
+					VK_API_VERSION_MAJOR(physicalDeviceProperties.apiVersion),
+					VK_API_VERSION_MINOR(physicalDeviceProperties.apiVersion),
+					VK_VERSION_PATCH(physicalDeviceProperties.apiVersion));
+
+			if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+				self->vulkanGlobals->physicalDevice = physicalDevice;
+				LOGGER_INFO("picked a discrete gpu: %s\n", physicalDeviceProperties.deviceName);
+			} else if (!fallbackPhysicalDevice) {
+				fallbackPhysicalDevice = physicalDevice;
+				LOGGER_INFO("picked %s as the fallback physical device\n", physicalDeviceProperties.deviceName);
+			}
+		}
+	}
+
+	if (!self->vulkanGlobals->physicalDevice && fallbackPhysicalDevice) {
+		self->vulkanGlobals->physicalDevice = fallbackPhysicalDevice;
+		LOGGER_INFO("picked the fallback physical device\n");
+	}
+
+	free(physicalDeviceGroupsProperties);
+	return VULKAN_RENDERER_API_OK;
+}
+
 VulkanRendererApiError vulkanRendererApiCreate(
 		VulkanRendererApi* self, Window* window)
 {
@@ -70,6 +141,12 @@ VulkanRendererApiError vulkanRendererApiCreate(
 		return error;
 	}
 	volkLoadInstance(self->vulkanGlobals->instance);
+
+	error = sVulkanPickPhysicalDevice(self);
+	if (error != VULKAN_RENDERER_API_OK) {
+		LOGGER_ERROR("could not pick a physical device: %d\n", error);
+		return error;
+	}
 	return VULKAN_RENDERER_API_OK;
 }
 

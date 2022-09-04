ce5fb8ec5fd277c11486b7dbcdddf4dc31ee27a7
Nikita Romanyuk <kelvium@yahoo.com>
2022-09-04 18:14:04 +0300

    VulkanRendererApi: implement instance creation

--- include/pogona/Renderer/VulkanRendererApi.h
+++ include/pogona/Renderer/VulkanRendererApi.h
@@ -11,15 +11,21 @@
 #endif
 #include <volk.h>
 
+typedef struct VulkanGlobals VulkanGlobals;
+
 typedef struct {
 	Window* window;
+	VulkanGlobals* vulkanGlobals;
 } VulkanRendererApi;
 
 typedef enum {
 	VULKAN_RENDERER_API_OK,
+	VULKAN_RENDERER_REQUIRED_VULKAN_VERSION_IS_NOT_SUPPORTED,
+	VULKAN_RENDERER_COULD_NOT_CREATE_INSTANCE,
 } VulkanRendererApiError;
 
-VulkanRendererApiError vulkanRendererApiCreate(VulkanRendererApi* self, Window* window);
+VulkanRendererApiError vulkanRendererApiCreate(
+		VulkanRendererApi* self, Window* window);
 VulkanRendererApiError vulkanRendererApiDestroy(VulkanRendererApi* self);
 
 #endif
--- src/Renderer/VulkanRendererApi.c
+++ src/Renderer/VulkanRendererApi.c
@@ -4,21 +4,80 @@
 
 #include <pch.h>
 #include <pogona/Renderer/VulkanRendererApi.h>
+#include <pogona/logger.h>
+
+typedef struct VulkanGlobals {
+	VkInstance instance;
+} VulkanGlobals;
+
+static VulkanRendererApiError sVulkanCreateInstance(VulkanRendererApi* self)
+{
+	u32 apiVersion;
+	vkEnumerateInstanceVersion(&apiVersion);
+	LOGGER_INFO("supported vulkan version: %d.%d.%d\n",
+			VK_API_VERSION_MAJOR(apiVersion), VK_API_VERSION_MINOR(apiVersion),
+			VK_API_VERSION_PATCH(apiVersion));
+
+	u32 requiredVersion = VK_API_VERSION_1_1;
+	if (apiVersion < requiredVersion) {
+		LOGGER_ERROR("required vulkan version is not supported: %d < %d\n",
+				apiVersion, requiredVersion);
+		return VULKAN_RENDERER_REQUIRED_VULKAN_VERSION_IS_NOT_SUPPORTED;
+	}
+
+	char applicationNameBuffer[256] = { 0 };
+	windowGetTitle(
+			self->window, applicationNameBuffer, sizeof(applicationNameBuffer));
+
+	const VkApplicationInfo applicationInfo = {
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pApplicationName = applicationNameBuffer,
+		.applicationVersion = 1, /* TODO: get the real value from somewhere :) */
+		.pEngineName = "pogona",
+		.engineVersion = 1, /* TODO: get the real value from somewhere :) */
+	};
+
+	const VkInstanceCreateInfo instanceCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &applicationInfo,
+		.enabledExtensionCount = 0,
+		.ppEnabledExtensionNames = NULL,
+		.enabledLayerCount = 0,
+		.ppEnabledLayerNames = NULL,
+	};
+
+	VkResult result = vkCreateInstance(
+			&instanceCreateInfo, NULL, &self->vulkanGlobals->instance);
+	if (result != VK_SUCCESS) {
+		LOGGER_ERROR("could not create a vulkan instance: %d\n", result);
+		return VULKAN_RENDERER_COULD_NOT_CREATE_INSTANCE;
+	}
+	return VULKAN_RENDERER_API_OK;
+}
 
 VulkanRendererApiError vulkanRendererApiCreate(
 		VulkanRendererApi* self, Window* window)
 {
 	self->window = window;
+	self->vulkanGlobals = calloc(1, sizeof(VulkanGlobals));
 
-	// TODO: implement
+	volkInitialize();
+
+	VulkanRendererApiError error;
+	error = sVulkanCreateInstance(self);
+	if (error != VULKAN_RENDERER_API_OK) {
+		LOGGER_ERROR("could not create an instance: %d\n", error);
+		return error;
+	}
+	volkLoadInstance(self->vulkanGlobals->instance);
 	return VULKAN_RENDERER_API_OK;
 }
 
 VulkanRendererApiError vulkanRendererApiDestroy(VulkanRendererApi* self)
 {
-	(void) self;
+	vkDestroyInstance(self->vulkanGlobals->instance, NULL);
 
-	// TODO: implement
+	free(self->vulkanGlobals);
 	return VULKAN_RENDERER_API_OK;
 }
 
